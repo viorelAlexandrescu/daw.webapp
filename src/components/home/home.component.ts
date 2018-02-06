43516fa50dc406dd04bae3f219b6789e696ad41e
Viorel Alexandrescu <alex.viorel.117@gmail.com>
2018-02-06 11:05:26 +0200

    added homepage component

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component, Inject } from '@angular/core';
 import { Http } from '@angular/http';
 import { Movie } from '../../models/Movie';
+import { CollectionService } from '../../services/collection.service';
 @Component({
     selector: 'home',
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
     latestMovies: Movie[];
+
+    constructor(private coll: CollectionService){
+        this.coll.getLatestMovies()
+                 .subscribe(latest => this.latestMovies = latest);
+    }
 }
