6d1732934a2d8340c954e0150782b8ec8ed5d484
Viorel Alexandrescu <alex.viorel.117@gmail.com>
2018-02-06 11:03:29 +0200

    added collection component

--- src/components/collection/collection.component.ts
+++ src/components/collection/collection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../models/Movie';
+import { CollectionService } from '../../services/collection.service';
 
 @Component({
   selector: 'app-collection',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionComponent implements OnInit {
 
-  constructor() { }
+  private movies: Movie[];
+  constructor(private coll: CollectionService) { }
 
   ngOnInit() {
+    this.coll.getAllMovies()
+             .subscribe(movies => this.movies = movies);
   }
 
 }
